5d8181b87e108b58fd7264e2e305c646cd09d4d8
Matheus Mercadante <zpunchpvp@gmail.com>
2021-06-18 13:59:54 -0300

    mongo cloud configurations

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DealModule } from '@components/deal/deal.module';
 import { OrderModule } from '@components/order/order.module';
 import { ConsolidationModule } from '@components/consolidation/consolidation.module';
-import { ormConfig } from '@database/config/ormconfig';
+
+// import { ormConfig } from '@database/config/ormconfig'
+
+const mongoConfig = process.env.DATABASE_HOST
+  ? `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+  : 'mongodb+srv://root:root@link-api.clm98.mongodb.net/myapi_db?retryWrites=true&w=majority';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(
-      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
-    ),
+    MongooseModule.forRoot(mongoConfig),
+    // MongooseModule.forRoot(
+    //   `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    // ),
+    // MongooseModule.forRoot(
+    //   `mongodb+srv://root:root@link-api.clm98.mongodb.net/myapi_db?retryWrites=true&w=majority`,
+    // ),
     // TypeOrmModule.forRoot(ormConfig()),
     DealModule,
     OrderModule,
--- api/src/database/config/ormconfig.ts
+++ api/src/database/config/ormconfig.ts
@@ -1,3 +1,11 @@
+export function mongoConfig(): string {
+  if (!process.env.DATABASE_HOST) {
+    return `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+  } else {
+    return 'mongodb+srv://root:root@link-api.clm98.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  }
+}
+
 export function ormConfig(): any {
   return {
     type: process.env.DATABASE_TYPE,
